57b1ca4995be354486c4c891b172128af5f14950
Miguel III <matercero@gmail.com>
2017-05-26 12:52:39 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -4,10 +4,10 @@ class DATABASE_CONFIG {
 	var $default = array(
 		'driver' => 'mysql',
 		'persistent' => false,
-		'host' => 'daferapp.talleresdafer.com',
-		'login' => 'dafer02',
-		'password' => 'd4f3r#beta',
-		'database' => 'daferBeta',
+		'host' => '',
+		'login' => '',
+		'password' => '',
+		'database' => '',
 		'encoding' => 'utf8'
 	);
 }
